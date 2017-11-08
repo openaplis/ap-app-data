57ed792388defbe77ea40be04c6667025508c005
SidHarder <softwarenavigator@gmail.com>
2017-11-08 13:35:22 -0700

    setting up app data

--- redis/update-redis.js
+++ redis/update-redis.js
@@ -7,19 +7,9 @@ var client = redis.createClient()
 var dataFolderPath = path.resolve('./data')
 var mappingFilePath = path.resolve('./data/mapping.json')
 
-client.on("error", function (err) {
-    console.log("Error " + err)
-})
-
-/*
-client.send_command("json.set", [3, '.', '{"name": "BOB"}'], function (err, result) {
-  if(err) return console.log(err)
-  client.send_command("json.get", [3], function (err, result) {
-    if(err) return console.log(err)
-    console.log(result)
-  })
-})
-*/
+//client.on("error", function (err) {
+//    console.log("Error " + err)
+//})
 
 fs.readFile(mappingFilePath, 'utf8', function (err, data) {
   var folderMappings = JSON.parse(data)
@@ -29,13 +19,21 @@ fs.readFile(mappingFilePath, 'utf8', function (err, data) {
       async.each(files, function (file, callback) {
         var objFilePath = path.join(folderPath, file)
         fs.readFile(objFilePath, 'utf8', function (err, data) {
+          if(err) return console.log(err)
           var obj = JSON.parse(data)
           client.send_command("json.set", [obj[mapping.key], '.', JSON.stringify(obj)], function (err, result) {
             if(err) return console.log(err)
-            console.log(obj[mapping.key] + " sent")
+            console.log(obj[mapping.key] + " set")
+            callback()
           })
         })
+      }, function (err) {
+        console.log('Finished looping files')
+        callback()
       })
     })
+  }, function (err) {
+    console.log('Finished looping folders, quitting')
+    client.quit()
   })
 })
