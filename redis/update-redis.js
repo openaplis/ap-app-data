616d743f4288b0424331cbe98de3a4fabdde4b9c
SidHarder <softwarenavigator@gmail.com>
2017-11-10 09:19:26 -0700

    flushall

--- redis/update-redis.js
+++ redis/update-redis.js
@@ -11,29 +11,33 @@ var mappingFilePath = path.resolve('./data/mapping.json')
 //    console.log("Error " + err)
 //})
 
-fs.readFile(mappingFilePath, 'utf8', function (err, data) {
-  var folderMappings = JSON.parse(data)
-  async.each(folderMappings, function(mapping, callback) {
-    var folderPath = path.join(dataFolderPath, mapping.folder)
-    fs.readdir(folderPath, function (err, files) {
-      async.each(files, function (file, callback) {
-        var objFilePath = path.join(folderPath, file)
-        fs.readFile(objFilePath, 'utf8', function (err, data) {
-          if(err) return console.log(err)
-          var obj = JSON.parse(data)
-          client.send_command("json.set", [obj[mapping.key], '.', JSON.stringify(obj)], function (err, result) {
+client.send_command('flushall', function (err, result) {
+  if(err) return console.log(err)
+  console.log("FLUSHALL: " + result)
+  fs.readFile(mappingFilePath, 'utf8', function (err, data) {
+    var folderMappings = JSON.parse(data)
+    async.each(folderMappings, function(mapping, callback) {
+      var folderPath = path.join(dataFolderPath, mapping.folder)
+      fs.readdir(folderPath, function (err, files) {
+        async.each(files, function (file, callback) {
+          var objFilePath = path.join(folderPath, file)
+          fs.readFile(objFilePath, 'utf8', function (err, data) {
             if(err) return console.log(err)
-            console.log(obj[mapping.key] + " set")
-            callback()
+            var obj = JSON.parse(data)
+            client.send_command("json.set", [obj[mapping.key], '.', JSON.stringify(obj)], function (err, result) {
+              if(err) return console.log(err)
+              console.log(obj[mapping.key] + " set")
+              callback()
+            })
           })
+        }, function (err) {
+          console.log('Finished looping files')
+          callback()
         })
-      }, function (err) {
-        console.log('Finished looping files')
-        callback()
       })
+    }, function (err) {
+      console.log('Finished looping folders, quitting')
+      client.quit()
     })
-  }, function (err) {
-    console.log('Finished looping folders, quitting')
-    client.quit()
   })
 })
