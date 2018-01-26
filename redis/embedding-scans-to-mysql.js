6ea8ab13c171bf896ec7b5046859ab2858b4942e
William Copland <william.copland@ypii.com>
2018-01-26 11:01:17 -0700

    Add embedding scans to mysql

---
+++ redis/embedding-scans-to-mysql.js
@@ -0,0 +1,39 @@
+var path = require('path')
+var fs = require('fs')
+var redis = require('redis')
+var async = require('async')
+const cmdSubmitter = require('ap-mysql').cmdSubmitter
+
+var client = redis.createClient('6379', '10.1.2.25')  //depreciated
+
+client.keys('EmbeddingScan:*', function(err, keys) {
+  if(err) console.log(err)
+  async.eachSeries(keys, function(key, callback) {
+    client.hvals(key, function(err, result) {
+      if(err) return(err)
+      var obj = {}
+      obj.AliquotOrderId = result[0]
+      obj.ProcessorStartTime = result[1]
+      obj.ProcessorFixationDuration = result[2]
+      obj.DateScanned = result[3]
+      obj.ScannedById = result[4]
+      obj.ScannedBy = result[5]
+      obj.Updated = result[6]
+      var objString = JSON.stringify(obj)
+      var date = new Date(result[3])
+      var mo = date.getMonth() + 1
+      var dt =  '' + date.getFullYear() + '-' + mo + '-' + date.getDate()
+      var cmd = ['insert tblEmbeddingScan(EmbeddingScan, DateScanned, AliquotOrderId) values (\'',
+        objString, '\', \'', dt, '\', \'', result[0], '\');'].join('')
+      cmdSubmitter.submit(cmd, function(err, result) {
+        if(err) return(err)
+        callback()
+      })
+    })
+
+  }, function (err) {
+    if(err) console.log(err)
+    client.quit()
+    console.Log("Finished")
+  })
+})
