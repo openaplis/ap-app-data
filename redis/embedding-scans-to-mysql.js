bbb931c295b620c6b1da950ed74dc64dd852031e
WilliamCopland <william.copland@ypii.com>
2018-05-21 10:14:33 -0600

    stains from BenchMark

--- redis/embedding-scans-to-mysql.js
+++ redis/embedding-scans-to-mysql.js
@@ -11,34 +11,38 @@ var errorCnt = 0
 client.keys('EmbeddingScan:*', function(err, keys) {
   if(err) console.log(err)
   async.eachSeries(keys, function(key, callback) {
-    client.hvals(key, function(err, result) {
-      if(err) return(err)
-      var obj = {}
-      obj.AliquotOrderId = result[0]
-      obj.ProcessorStartTime = result[1]
-      obj.ProcessorFixationDuration = result[2]
-      obj.DateScanned = result[3]
-      obj.ScannedById = result[4]
-      obj.ScannedBy = result[5]
-      obj.Updated = result[6]
-      var objString = JSON.stringify(obj)
-      var date = new Date(result[3])
-      var mo = date.getMonth() + 1
-      var dt =  '' + date.getFullYear() + '-' + mo + '-' + date.getDate()
-      var cmd = ['insert tblEmbeddingScan(EmbeddingScan, DateScanned, AliquotOrderId) values (\'',
-        objString, '\', \'', dt, '\', \'', result[0], '\');'].join('')
-      cmdSubmitter.submit(cmd, function(err, result) {
-        if(err) {
-          errorCnt = errorCnt + 1
-          console.log("Error: " + errorCnt)
-          callback()
-        } else {
-          cnt = cnt + 1
-          console.log(cnt)
-          callback()
-        }
+    if(cnt > 999) callback("up to count")
+    else {
+      client.hvals(key, function(err, result) {
+        if(err) return(err)
+        var obj = {}
+        obj.AliquotOrderId = result[0]
+        obj.ProcessorStartTime = result[1]
+        obj.ProcessorFixationDuration = result[2]
+        obj.DateScanned = result[3]
+        obj.ScannedById = result[4]
+        obj.ScannedBy = result[5]
+        obj.Updated = result[6]
+        var objString = JSON.stringify(obj)
+        var date = new Date(result[3])
+        var mo = date.getMonth() + 1
+        var dt =  '' + date.getFullYear() + '-' + mo + '-' + date.getDate()
+        var cmd = ['insert tblEmbeddingScan(EmbeddingScan, DateScanned, AliquotOrderId) values (\'',
+          objString, '\', \'', dt, '\', \'', result[0], '\');'].join('')
+        cmdSubmitter.submit(cmd, function(err, result) {
+          if(err) {
+            errorCnt = errorCnt + 1
+            console.log("Error: " + errorCnt + " - " + obj.AliquotOrderId)
+            console.log(err)
+            callback()
+          } else {
+            cnt = cnt + 1
+            //console.log(cnt)
+            callback()
+          }
+        })
       })
-    })
+    }
   }, function (err) {
     if(err) console.log(err)
     client.quit()
